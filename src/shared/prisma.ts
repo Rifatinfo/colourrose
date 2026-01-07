448896c9f90f312a658d89e008db2f386cdc2b6e
Rifatinfo <mdrifathossainsinfo@gmail.com>
2026-01-07 18:33:38 +0600

    add latest code

--- src/shared/prisma.ts
+++ src/shared/prisma.ts
@@ -1,37 +1,48 @@
-import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaClient } from "@prisma/client";
+// import { Pool } from "pg";
+
+//===================== Prisma Client Setup =====================//
+import "dotenv/config";
+import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from "@prisma/client";
-import { Pool } from "pg";
+// import { PrismaClient } from '../generated/prisma/client'
 
+//===================== PostgreSQL Connection Pool Setup =====================//
 // Create PostgreSQL connection pool with optimized settings
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    max: 20, // Maximum number of connections in the pool
-    idleTimeoutMillis: 30000, // Close idle connections after 30 seconds
-    connectionTimeoutMillis: 10000, // Return error after 10 seconds if unable to connect
-});
-const adapter = new PrismaPg(pool);
+// const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//     max: 20, // Maximum number of connections in the pool
+//     idleTimeoutMillis: 30000, // Close idle connections after 30 seconds
+//     connectionTimeoutMillis: 10000, // Return error after 10 seconds if unable to connect
+// });
+// const adapter = new PrismaPg(pool);
+const connectionString = `${process.env.DATABASE_URL}`
 
-const prisma = new PrismaClient({
-    adapter,
-    log: [
-        {
-            emit: 'event',
-            level: 'query',
-        },
-        {
-            emit: 'event',
-            level: 'error',
-        },
-        {
-            emit: 'event',
-            level: 'info',
-        },
-        {
-            emit: 'event',
-            level: 'warn',
-        },
-    ],
-})
+const adapter = new PrismaPg({ connectionString })
+const prisma = new PrismaClient({ adapter })
+
+// const prisma = new PrismaClient({
+//     adapter,
+//     log: [
+//         {
+//             emit: 'event',
+//             level: 'query',
+//         },
+//         {
+//             emit: 'event',
+//             level: 'error',
+//         },
+//         {
+//             emit: 'event',
+//             level: 'info',
+//         },
+//         {
+//             emit: 'event',
+//             level: 'warn',
+//         },
+//     ],
+// })
 
 // prisma.$on('query', (e) => {
 //     // console.log("-------------------------------------------")
